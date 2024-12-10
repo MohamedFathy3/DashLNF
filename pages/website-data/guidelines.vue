3b91c38c002b5f0039e5cb13acd5324958793d0e
codaski <amaher.code@gmail.com>
2024-12-10 18:19:07 +0200

    add guide lines crud for lnf dashboard

---
+++ pages/website-data/guidelines.vue
@@ -0,0 +1,414 @@
+<script setup>
+import { numeric, required } from '@vuelidate/validators';
+import useVuelidate from '@vuelidate/core';
+
+definePageMeta({
+    middleware: 'auth',
+});
+const selectedRows = ref([]);
+const sortByList = ref([
+    { name: 'Sort By ID', value: 'id' },
+    { name: 'Sort By Title', value: 'title' },
+]);
+const filter = ref({
+    name: null,
+});
+const serverParams = ref({
+    filters: {},
+    orderBy: 'id',
+    orderByDirection: 'desc',
+    perPage: 25,
+    page: 1,
+    paginate: true,
+    deleted: false,
+});
+const settingStore = useSettingsStore();
+const formLoading = ref(false);
+const isOpen = ref(false);
+const editMode = ref(false);
+const resetServerParams = async () => {
+    filter.value = {
+        title: null,
+    };
+    serverParams.value = {
+        filters: {},
+        orderBy: 'id',
+        orderByDirection: 'desc',
+        perPage: 25,
+        page: 1,
+        paginate: true,
+        deleted: false,
+    };
+    selectedRows.value = [];
+    await refresh();
+};
+const {
+    data: rows,
+    pending,
+    refresh,
+} = await useApiFetch('/api/guide-line/index', {
+    method: 'POST',
+    body: serverParams,
+    lazy: true,
+});
+watch(
+    filter,
+    (newVal) => {
+        for (const key in newVal) {
+            const value = newVal[key];
+            if (value) {
+                serverParams.value.filters[key] = value;
+            } else {
+                delete serverParams.value.filters[key];
+            }
+        }
+    },
+    { deep: true },
+);
+const toggleDeleted = async () => {
+    serverParams.value.deleted = !serverParams.value.deleted;
+    selectedRows.value = [];
+    await refresh();
+};
+const isSelected = (id) => {
+    return selectedRows.value.some((r) => r === id);
+};
+const allSelected = computed(() => {
+    return rows?.value?.data.every((row) => selectedRows.value.includes(row.id));
+});
+const selectAllRows = () => {
+    const allSelected = rows.value.data.every((row) => isSelected(row.id));
+    if (allSelected) {
+        selectedRows.value = [];
+    } else {
+        rows.value.data.forEach((row) => {
+            const id = row.id;
+            if (!isSelected(id)) {
+                selectedRows.value.push(id);
+            }
+        });
+    }
+};
+const changePage = async (value) => {
+    const pageNumber = parseInt(value);
+    if (!isNaN(pageNumber)) {
+        serverParams.value.page = pageNumber;
+    } else {
+        console.error('Invalid page number:', value);
+    }
+    selectedRows.value = [];
+    await refresh();
+};
+const toggleRowSelection = (id) => {
+    const index = selectedRows.value.indexOf(id);
+    if (index === -1) {
+        selectedRows.value.push(id);
+    } else {
+        selectedRows.value.splice(index, 1);
+    }
+};
+const item = ref({
+    title: null,
+    active: true,
+    description: null,
+    position: null,
+});
+const rules = ref({
+    title: { required },
+    active: {},
+    description: {},
+    position: { numeric },
+});
+const v$ = useVuelidate(rules, item);
+const fetchItem = async (id) => {
+    const { data, error } = await useApiFetch(`/api/guide-line/${id}`, {
+        lazy: true,
+    });
+    if (data.value) {
+        item.value = data.value.data;
+    }
+    if (error.value) {
+        useToast({ title: 'Error', message: error.value.message, type: 'error', duration: 5000 });
+    }
+};
+const resetItemValues = async () => {
+    item.value = {
+        title: null,
+        active: true,
+        description: null,
+        position: null,
+    };
+};
+
+async function closeModal() {
+    isOpen.value = false;
+    editMode.value = false;
+    v$.value.$reset();
+    await resetItemValues();
+}
+
+async function openModal(id = null) {
+    formLoading.value = true;
+    if (id !== null) {
+        editMode.value = true;
+        await fetchItem(id);
+    } else {
+        editMode.value = false;
+    }
+    formLoading.value = false;
+    isOpen.value = true;
+}
+
+async function updateItem() {
+    const { data, error } = await useApiFetch(`/api/guide-line/${item.value?.id}`, {
+        method: 'PATCH',
+        body: item,
+        lazy: true,
+    });
+    if (data.value) {
+        useToast({ title: 'Success', message: data.value.message, type: 'success', duration: 5000 });
+        await closeModal();
+        await refresh();
+    }
+    if (error.value) {
+        useToast({ title: 'Error', message: error.value.data.message ?? error.value.message, type: 'error', duration: 5000 });
+    }
+}
+
+async function addItem() {
+    const { data, error } = await useApiFetch(`/api/guide-line`, {
+        method: 'POST',
+        body: item,
+        lazy: true,
+    });
+    if (data.value) {
+        useToast({ title: 'Success', message: data.value.message, type: 'success', duration: 5000 });
+        await closeModal();
+        await refresh();
+    }
+    if (error.value) {
+        useToast({ title: 'Error', message: error.value.data.message ?? error.value.message, type: 'error', duration: 5000 });
+    }
+}
+
+async function handleModalSubmit() {
+    formLoading.value = true;
+    const result = await v$.value.$validate();
+    if (!result) {
+        formLoading.value = false;
+        useToast({ title: 'Error', message: 'Please fill all required inputs', type: 'error', duration: 5000 });
+        return false;
+    }
+    if (editMode.value === true) {
+        await updateItem();
+    } else {
+        await addItem();
+    }
+}
+
+async function deleteItems() {
+    const confirmed = confirm('Are you sure you want to delete this item?');
+    if (confirmed) {
+        const { data, error } = await useApiFetch(`/api/guide-line/delete`, {
+            body: { items: selectedRows.value },
+            method: 'DELETE',
+            lazy: true,
+        });
+        if (data.value) {
+            useToast({ title: 'Success', message: data.value.message, type: 'success', duration: 5000 });
+            await refresh();
+        }
+        if (error.value) {
+            useToast({ title: 'Error', message: error.value.message, type: 'error', duration: 5000 });
+        }
+    }
+}
+
+async function forceDeleteItems() {
+    const confirmed = confirm('Are you sure you want to delete this item?');
+    if (confirmed) {
+        const { data, error } = await useApiFetch(`/api/guide-line/force-delete`, {
+            body: { items: selectedRows.value },
+            method: 'DELETE',
+            lazy: true,
+        });
+        if (data.value) {
+            useToast({ title: 'Success', message: data.value.message, type: 'success', duration: 5000 });
+            await refresh();
+        }
+        if (error.value) {
+            useToast({ title: 'Error', message: error.value.message, type: 'error', duration: 5000 });
+        }
+    }
+}
+
+async function restoreItems() {
+    const confirmed = confirm('Are you sure you want to delete this item?');
+    if (confirmed) {
+        const { data, error } = await useApiFetch(`/api/guide-line/restore`, {
+            body: { items: selectedRows.value },
+            method: 'POST',
+            lazy: true,
+        });
+        if (data.value) {
+            useToast({ title: 'Success', message: data.value.message, type: 'success', duration: 5000 });
+            await refresh();
+        }
+        if (error.value) {
+            useToast({ title: 'Error', message: error.value.message, type: 'error', duration: 5000 });
+        }
+    }
+}
+</script>
+<template>
+    <div class="flex flex-col gap-8">
+        <!-- Page Title & Action Buttons -->
+        <div class="md:flex md:items-center md:justify-between md:gap-5">
+            <div class="flex items-center gap-2">
+                <Icon name="solar:asteroid-linear" class="size-5 opacity-75" />
+                <div>{{ serverParams.deleted ? 'Deleted Guidelines' : 'Guidelines' }}</div>
+            </div>
+            <div class="md:flex md:items-center md:gap-5 md:space-y-0 space-y-5">
+                <template v-if="selectedRows.length > 0">
+                    <button v-if="serverParams.deleted" class="btn btn-danger btn-rounded px-6 btn-sm gap-3 md:w-fit w-full md:mt-0 mt-5" @click="forceDeleteItems">
+                        <Icon name="solar:trash-bin-minimalistic-line-duotone" class="size-5 opacity-75" />
+                        Delete Permanently
+                    </button>
+                    <button v-else class="btn btn-danger btn-rounded px-6 btn-sm gap-3 md:w-fit w-full md:mt-0 mt-5" @click="deleteItems">
+                        <Icon name="solar:trash-bin-minimalistic-line-duotone" class="size-5 opacity-75" />
+                        Delete Items
+                    </button>
+                    <button v-if="serverParams.deleted" class="btn btn-success btn-rounded px-6 btn-sm gap-3 md:w-fit w-full md:mt-0 mt-5" @click="restoreItems">
+                        <Icon name="solar:restart-circle-outline" class="size-5 opacity-75" />
+                        Restore Items
+                    </button>
+                </template>
+                <button :disabled="serverParams.deleted" class="btn btn-primary btn-rounded px-6 btn-sm gap-3 md:w-fit w-full md:mt-0 mt-5" @click="openModal()">
+                    <Icon name="solar:add-square-linear" class="size-5 opacity-75" />
+                    Add New
+                </button>
+                <button class="btn btn-primary btn-rounded px-6 btn-sm gap-3 md:w-fit w-full md:mt-0 mt-5" @click="toggleDeleted">
+                    <Icon :name="serverParams.deleted ? 'solar:hamburger-menu-line-duotone' : 'solar:trash-bin-minimalistic-line-duotone'" class="size-5 opacity-75" />
+                    {{ serverParams.deleted ? 'Items List' : 'Deleted Items' }}
+                </button>
+            </div>
+        </div>
+        <!-- Filter & Search -->
+        <div class="grid lg:grid-cols-12 gap-5 items-center p-5 bg-white border rounded-2xl">
+            <FormInputField v-model="filter.title" rounded class="xl:col-span-4 lg:col-span-4" placeholder="Name" />
+            <FormSelectField v-model="serverParams.orderBy" :clearable="false" class="xl:col-span-4 lg:col-span-4" labelvalue="name" keyvalue="value" placeholder="Sort Direction" :select-data="sortByList" />
+            <FormSelectField
+                v-model="serverParams.orderByDirection"
+                class="xl:col-span-4 lg:col-span-4"
+                :clearable="false"
+                labelvalue="name"
+                keyvalue="value"
+                placeholder="Sort Direction"
+                :select-data="[
+                    { name: 'Z : A', value: 'desc' },
+                    { name: 'A : Z', value: 'asc' },
+                ]"
+            />
+            <button class="xl:col-span-6 lg:col-span-6 btn btn-rounded btn-sm btn-primary gap-3 w-full" @click="refresh">
+                <Icon name="solar:rounded-magnifer-line-duotone" class="size-5 shrink-0" />
+                Filter
+            </button>
+            <button class="xl:col-span-6 lg:col-span-6 btn btn-rounded btn-sm btn-secondary gap-3 w-full" @click="resetServerParams">
+                <Icon name="solar:restart-circle-outline" class="size-5 shrink-0" />
+                Reset
+            </button>
+        </div>
+        <!-- Table -->
+        <table class="table table-report font-light">
+            <thead>
+                <tr class="uppercase text-sm">
+                    <th class="text-left">
+                        <input v-model="allSelected" type="checkbox" class="form-check-input" @change="selectAllRows" />
+                    </th>
+                    <th class="text-left">Name</th>
+                    <th class="text-center">Position</th>
+                    <th class="text-center">Active</th>
+                    <th v-if="serverParams.deleted">Deleted At</th>
+                    <th class="text-right">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-if="!pending && rows">
+                    <tr v-for="row in rows.data" :key="row.id" class="text-sm">
+                        <td>
+                            <input :checked="isSelected(row.id)" type="checkbox" class="form-check-input" @change="toggleRowSelection(row.id)" />
+                        </td>
+                        <td>
+                            <div class="opacity-75 font-medium line-clamp-1">{{ row.title }}</div>
+                        </td>
+                        <td class="text-center">
+                            {{ row.position }}
+                        </td>
+                        <td>
+                            <div class="flex items-center place-content-center">
+                                <FormSwitch :id="'row-active-' + row.id" v-model="row.active" :disabled="serverParams.deleted" @change="useToggleSwitch(row.id, 'active', 'termsCondition')" />
+                            </div>
+                        </td>
+                        <td v-if="serverParams.deleted" class="text-sm">{{ row.deletedAt }}</td>
+                        <td class="text-right">
+                            <div>
+                                <button :disabled="serverParams.deleted" class="btn btn-secondary btn-rounded btn-sm gap-3" @click="openModal(row.id)">
+                                    <Icon name="solar:pen-new-round-outline" class="size-4" />
+                                    Edit
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </template>
+                <template v-else>
+                    <tr v-for="i in serverParams.perPage" :key="i">
+                        <td colspan="7">
+                            <div class="h-12 !opacity-50 animate-pulse" />
+                        </td>
+                    </tr>
+                </template>
+                <template v-if="!pending && rows && rows.data.length === 0">
+                    <tr>
+                        <td colspan="7">
+                            <div class="text-center">
+                                <Icon name="solar:cloud-check-line-duotone" class="size-20 opacity-50" />
+                                <div class="text-sm mt-5 opacity-75">No Data found</div>
+                            </div>
+                        </td>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
+        <!-- Pagination -->
+        <TablePagination :pending="pending" :rows="rows" :page="serverParams.page" @change-page="changePage" />
+
+        <TheModal :open-modal="isOpen" size="5xl" @close-modal="closeModal()">
+            <template #header>
+                <div class="flex justify-between items-center">
+                    <div class="font-medium" v-html="editMode ? 'Update Item' : 'Add New Item'"></div>
+                    <Icon class="w-8 h-8 opacity-50 cursor-pointer hover:opacity-100 ease-in-out duration-300" name="solar:close-square-outline" @click="closeModal" />
+                </div>
+            </template>
+            <template #content>
+                <div class="grid lg:grid-cols-12 gap-5 items-start">
+                    <FormInputField v-model="item.title" :errors="v$.title.$errors" class="lg:col-span-12" label="Title" name="title" placeholder="Title" />
+                    <FormRichTextEditor v-model="item.description" :errors="v$.description.$errors" label="Description" name="description" class="col-span-12" />
+                    <FormSwitch v-model="item.active" class="col-span-12 sm:col-span-4" flex-title label="Active" name="active-toggle" />
+                    <FormInputField v-model="item.position" :errors="v$.position.$errors" class="lg:col-span-9" label="Position" name="order-id" placeholder="Position Number" type="number" />
+                </div>
+            </template>
+            <template #footer>
+                <div class="w-full flex items-center justify-end gap-5">
+                    <button :disabled="formLoading" class="btn-rounded btn-sm btn btn-danger px-4" type="button" @click="closeModal">
+                        <Icon :name="formLoading ? 'svg-spinners:3-dots-fade' : 'solar:close-circle-linear'" class="w-5 h-5 mr-2" />
+                        <span>Close</span>
+                    </button>
+                    <button :disabled="formLoading" class="btn-rounded btn-sm btn btn-primary px-4" type="button" @click="handleModalSubmit()">
+                        <Icon :name="formLoading ? 'svg-spinners:3-dots-fade' : 'solar:check-circle-broken'" class="w-5 h-5 mr-2" />
+                        <span v-html="editMode ? 'Update' : 'Save'" />
+                    </button>
+                </div>
+            </template>
+        </TheModal>
+    </div>
+</template>
